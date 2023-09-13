0dcfc382ba668a65eebfdf23f699de48cc16cffc
DavidElPapu <david.jung@uniat.edu.mx>
2023-09-13 17:10:35 -0600

    Ya se mueve en vertical y horizontal el jugador

---
+++ Assets/Scripts/Player/PlayerMovement.cs
@@ -0,0 +1,70 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{
+    // Variables publicas
+    public float walkSpeed, runSpeed;
+    public bool canMove;
+
+    // Variables privadas
+    private Vector3 vectorMovement;
+    private float speed;
+    private CharacterController characterController;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        // Inicializacion de variables
+        characterController = GetComponent<CharacterController>();
+        speed = walkSpeed;
+        vectorMovement = Vector3.zero;
+        canMove = true;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(canMove)
+        {
+            Walk();
+            Run();
+        }
+        Gravity();
+    }
+
+    //Funcion para caminar
+    void Walk()
+    {
+        // Conseguimos los inputs
+        vectorMovement.x = Input.GetAxis("Horizontal");
+        vectorMovement.z = Input.GetAxis("Vertical");
+
+        // Normalizamos el vector de movimiento
+        vectorMovement = vectorMovement.normalized;
+
+        // Movemos al player
+        characterController.Move(vectorMovement * speed * Time.deltaTime);
+    }
+
+    // Funcion para correr
+    void Run()
+    {
+        // Si presionamos el boton para correr modificamos la velocidad
+        if(Input.GetAxis("Run") > 0f)
+        {
+            speed = runSpeed;
+        }
+        else
+        {
+            speed = walkSpeed;
+        }
+    }
+
+    // Funcion provicional de gravedad
+    void Gravity()
+    {
+        characterController.Move(new Vector3(0f, -4f * Time.deltaTime, 0f));
+    }
+}
